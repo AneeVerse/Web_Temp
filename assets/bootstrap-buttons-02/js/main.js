70709a34bf10b81b7b67a03a1e10b6e56cf5f2bb
Ahmad Anees <83137173+Anees101@users.noreply.github.com>
2022-08-29 00:27:38 -0700

    data Changed

--- assets/bootstrap-buttons-02/js/main.js
+++ assets/bootstrap-buttons-02/js/main.js
@@ -254,22 +254,3 @@
   });
 
 })()
-
-
-
-
-
-
-
-//Age
-function age() {
-
-var birth = new Date('2003-01-16');
-var now = new Date();
-var difference = now - birth;
-// Dates are set from 1970, so remove that to get the year 
-var age = new Date(difference).getFullYear() - 1970
-
-var span = document.getElementById('age');
-span.innerHTML = age;
-}
